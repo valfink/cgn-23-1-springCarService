8c820242733c03cc989b79bd7d2b2d8c34754673
valfink <valentin.finke@gmail.com>
2023-02-06 15:21:14 +0100

    Add CarController class

---
+++ src/main/java/com/example/springCarService/controller/CarController.java
@@ -0,0 +1,24 @@
+package com.example.springCarService.controller;
+
+import com.example.springCarService.model.Car;
+import com.example.springCarService.service.CarService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CarController {
+    private CarService carService = new CarService();
+    @PostMapping("car/new")
+    public Car postNewCar(@RequestBody Car car) {
+        carService.addCar(car);
+        return car;
+        // lame, should get the new car directly from the service!
+    }
+
+    @GetMapping("cars")
+    public List<Car> getAllCars() {
+        return carService.getCars();
+    }
+}
