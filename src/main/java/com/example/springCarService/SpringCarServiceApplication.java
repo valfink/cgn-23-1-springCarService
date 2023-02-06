defbeb9ff831ad95b0b3b24678b821ce8951e476
valfink <valentin.finke@gmail.com>
2023-02-06 15:10:16 +0100

    Initial commit

---
+++ src/main/java/com/example/springCarService/SpringCarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springCarService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCarServiceApplication.class, args);
+	}
+
+}
